0d79a431ce14d7a07964c415bd788460734f1c26
ippolito21 <60745255+ippolito21@users.noreply.github.com>
2020-03-25 17:10:58 +0100

    1er sauvegarde

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+function ouvrirFermerMenu(){
+    document.querySelector('menu').classList.toggle('ferme');
+}
+document.querySelector('.toggle-menu').addEventListener ('click', ouvrirFermerMenu);
+document.querySelector('.fermer-menu').addEventListener('click', ouvrirFermerMenu);
+function ouvrirFermerMenu(){
+    document.querySelector('menu').classList.toggle('ferme');
+});
